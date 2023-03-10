6d8f40bdaa97735b0ed6f593da11052be9ea5a12
Arslan Okenov <gtg100900@gmail.com>
2023-03-10 15:54:03 +0600

    HW#89- cleaning code and added functions to patch request

--- hw-api/routers/track_history.ts
+++ hw-api/routers/track_history.ts
@@ -1,24 +1,21 @@
 import express from "express";
 import User from "../models/User";
 import TrackHistory from "../models/TrackHistory";
+import auth from "../middleware/auth";
 
 const Track_HistoryRouter = express.Router();
 
-Track_HistoryRouter.get('/', async (req, res) => {
+Track_HistoryRouter.get('/',auth, async (req, res) => {
   const token = req.get('Authorization');
-  if (!token) {
-    return res.status(401).send({error: 'No token present'});
-  }
   try {
     const user = await User.findOne({token});
-    if (!user) {
-      return res.status(401).send({error: 'Wrong token!'});
+    if(user) {
+      const result = await TrackHistory.find({user:user._id.toString()}).sort({datetime:-1}).populate('track', 'name duration album');
+      return res.send(result);
     }
-
-    const result = await TrackHistory.find({user:user._id.toString()}).sort({datetime:-1}).populate('track', 'name duration album');
-    res.send(result);
   } catch (e) {
     console.log(e);
+
   }
 });
 
@@ -37,7 +34,6 @@ Track_HistoryRouter.post('/', async (req, res) => {
       return await TrackHistory.updateOne({
             user:user._id.toString(),
              track: req.body.track,
-
            },
            {datetime: new Date().toISOString()});
     }
